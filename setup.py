79afd20755e29fd7a6efeb48a2e137f1cdc81aac
QuantumCoder1024 <jianglie2023@gmail.com>
2025-08-03 09:18:29 +0800

    update

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Setup script for gym-super-mario-bros package."""
+
+from setuptools import setup
+
+# For compatibility with older tooling, all configuration is in pyproject.toml
+if __name__ == "__main__":
+    setup()
